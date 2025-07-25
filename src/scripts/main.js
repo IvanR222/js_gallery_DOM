19f1aa2332769a9eb689c1f4df33014ab55243a4
Ivan <rusev065@gmail.com>
2025-07-25 12:44:47 +0300

    add task Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (!link || !thumbs.contains(link)) {
+    return;
+  }
+
+  const largeImage = document.getElementById('largeImg');
+
+  largeImage.src = link.href;
+  largeImage.alt = link.title;
+});
